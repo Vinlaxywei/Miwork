de7746d074817f6e4fb902f58acdb6d1b16c5a2e
vinlaxy <andedexiang@gmail.com>
2016-08-15 22:12:07 +0800

    完成miwok 第一阶段应用功能

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -19,11 +19,18 @@ public class WordAdapter extends ArrayAdapter<Word> {
         super(context, 0, word);
     }
 
+    /*
+    * @param position 当前item位置
+    * @param convertView 当前传入的view
+    * @param parent 目标listview
+    * */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         View listItemView = convertView;
-        //检查item如果是空的，则进行填充
+        /*
+        * 检查item如果是空的，则创建一个新的word_list_item进行填充，填充到目标parent
+        * */
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.word_list_item, parent, false);
         }
